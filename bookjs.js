6a638e80bc008fac3cb98957b7faaec02abeddb1
MeliRoldan <81765847+MeliRoldan@users.noreply.github.com>
2022-08-14 13:49:11 +0400

    hbu

--- bookjs.js
+++ bookjs.js
@@ -1,14 +1,5 @@
 var myNav = document.getElementById("nav");
 
-window.onscroll = function() {
-  "use strict";
-  if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100) {
-    myNav.classList.add("scroll");
-  } else {
-    myNav.classList.remove("scroll");
-  }
-};
-
 const gudauri = {
   placeName: "Gudauri",
   placePrise: 250,
