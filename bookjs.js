b014a4b8c0a32d5d7e0ab4f292c3181cb0e7794f
MeliRoldan <81765847+MeliRoldan@users.noreply.github.com>
2023-03-03 22:05:56 +0400

    new version

--- bookjs.js
+++ bookjs.js
@@ -1,27 +1,29 @@
-var myNav = document.getElementById("nav");
+window.onload = function(){
+  var myNav = document.getElementById("nav");
 
-const gudauri = {
-  placeName: "Gudauri",
-  placePrise: 250,
+  window.onscroll = function() {
+    "use strict";
+    if (document.body.scrollTop >= 70 || document.documentElement.scrollTop >= 70) {
+      myNav.classList.add("scroll");
+    } else {
+      myNav.classList.remove("scroll");
+    }
+  };
 };
 
-const svaneti = {
-  placeName: "Svaneti",
-  placePrise: 350,
+const locations = ["Gudauri","Svaneti","Tbilisi","Gonio","Sighnaghi"];
+let options = document.getElementById("options");
+for (let i=0; i<locations.length; i++){
+  options.innerHTML += `<option value="${[i]}">${locations[i]}</option>`;
 };
 
-const tbilisi = {
-  placeName: "Tbilisi",
-  placePrise: 60,
-};
 
-const gonio = {
-  placeName: "Gonio",
-  placePrise: 150,
-};
 
-const sighnaghi = {
-  placeName: "Sighnaghi",
-  placePrise: 80,
-};
-
+  /*i should add array with the locations and run foreach to add the loctaions as options to the select tag
+   and i should create if conditions that will show the price in the corresponding tag based on the loctaions choosed.
+   Gudauri - 250
+   Svaneti - 350
+   Tbilisi - 60
+   Gonio - 120
+   Signaghi - 90
+   */
--- js.js
+++ js.js
@@ -2,9 +2,11 @@ var myNav = document.getElementById("nav");
 
 window.onscroll = function() {
   "use strict";
-  if (document.body.scrollTop >= 650 || document.documentElement.scrollTop >= 650) {
+  if (document.body.scrollTop >= 155 || document.documentElement.scrollTop >= 155) {
     myNav.classList.add("scroll");
   } else {
     myNav.classList.remove("scroll");
   }
 };
+
+$("indicator").hasClass("active").removeClass("indicator");
